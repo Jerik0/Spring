e05e7c65b9310a4ed27b913ce181f8fadc57ab13
Jeriko <jcarrico1985@live.com>
2017-06-21 12:14:10 -0500

    Refactored indentation of files from 4 spaces to 2 spaces. Add PostSvc.save method to PostsController method editPostForm

--- src/main/java/com/codeup/PostSvc.java
+++ src/main/java/com/codeup/PostSvc.java
@@ -6,30 +6,29 @@ import java.util.List;
 
 @Service("postSvc")
 public class PostSvc {
+  private List<Post> posts = new ArrayList<>();
 
-    private List<Post> posts = new ArrayList<>();
+  public PostSvc() {
+    createAds();
+  }
 
-    public PostSvc() {
-        createAds();
-    }
+  public List<Post> findAll() {
+    return posts;
+  }
 
-    public List<Post> findAll() {
-        return posts;
-    }
+  public Post save(Post post) {
+    post.setId( (long) posts.size() + 1);
+    posts.add(post);
+    return post;
+  }
 
-    public Post save(Post post) {
-        post.setId( (long) posts.size() + 1);
-        posts.add(post);
-        return post;
-    }
+  public Post findOne(long id) {
+    return posts.get((int) (id - 1));
+  }
 
-    public Post findOne(long id) {
-        return posts.get((int) (id - 1));
-    }
-
-    private void createAds() {
-        save(new Post("This is the title", "This is the body"));
-        save(new Post("This is also the title", "This is also the body"));
-    }
+  private void createAds() {
+    save(new Post("This is the title", "This is the body"));
+    save(new Post("This is also the title", "This is also the body"));
+  }
 }
 
--- src/main/java/com/codeup/controllers/PostsController.java
+++ src/main/java/com/codeup/controllers/PostsController.java
@@ -13,43 +13,61 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PostsController {
 
-    private final PostSvc postSvc;
+  private final PostSvc postSvc;
 
-    @Autowired
-    public PostsController(PostSvc postSvc) {
-        this.postSvc = postSvc;
-    }
+  @Autowired
+  public PostsController(PostSvc postSvc) {
+    this.postSvc = postSvc;
+  }
 
-    // When URL Pattern is "/posts", adds new post to Array List.
-    // Will be updated later to use MySQL insertions.
-    @GetMapping("/posts")
-    public String posts(Model model) {
+  // When URL Pattern is "/posts", adds new post to Array List.
+  // Will be updated later to use MySQL insertions.
+  @GetMapping("/posts")
+  public String posts(Model model) {
 
-        model.addAttribute("posts", postSvc.findAll());
-        return "posts/index";
-    }
+    model.addAttribute("posts", postSvc.findAll());
+    return "posts/index";
+  }
 
-    @GetMapping("/posts/{id}")
-    public String postIds(@PathVariable long id, Model model) {
+  @GetMapping("/posts/{id}")
+  public String postIds(@PathVariable long id, Model model) {
 
-        model.addAttribute("post", postSvc.findOne(id));
-        return "posts/show";
-    }
+    model.addAttribute("post", postSvc.findOne(id));
+    return "posts/show";
+  }
 
-    @GetMapping("/posts/create")
-    public String savePost(Model model) {
-        model.addAttribute("post", new Post());
-        return "posts/create";
-    }
+  @GetMapping("/posts/create")
+  public String savePost(Model model) {
+    model.addAttribute("post", new Post());
+    return "posts/create";
+  }
 
-    @PostMapping("/posts/create")
-    public String viewPostForm(
-            Model model,
-            @RequestParam(name = "title") String title,
-            @RequestParam(name = "body") String body
-    ) {
-        Post post = new Post(title, body);
-        model.addAttribute(post);
-        return "posts/create";
-    }
+  @PostMapping("/posts/create")
+  public String viewPostForm(
+          Model model,
+          @RequestParam(name = "title") String title,
+          @RequestParam(name = "body") String body
+  ) {
+    Post post = new Post(title, body);
+    model.addAttribute(post);
+    return "posts/create";
+  }
+
+  @GetMapping("/posts/edit")
+  public String EditPost(Model model) {
+    model.addAttribute("post", new Post());
+    return "posts/edit";
+  }
+
+  @PostMapping("/posts/edit")
+  public String editPostForm(
+          Model model,
+          @RequestParam(name = "title") String title,
+          @RequestParam(name = "body") String body
+  ) {
+    Post post = new Post(title, body);
+    postSvc.save(post);
+    model.addAttribute(post);
+    return "posts/edit";
+  }
 }
